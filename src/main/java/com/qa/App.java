0cdf934907f30e93bb5ae6b5d69637b345bd2231
Jax <jbraith29@gmail.com>
2024-11-04 15:48:46 +0000

    laganjaEstranja

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -13,7 +13,7 @@ public class App
         System.out.println(sayHelloToSomeone("Bob"));
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
-        System.out.println(sayHelloToSomeone("Bethany"));
+        System.out.println(sayHelloToSomeone("Season 6 let's get SICKENING"));
         System.out.println(sayGoodbye());
     }
 
