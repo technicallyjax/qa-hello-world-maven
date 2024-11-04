5ad8fb32a787eb9dfd34af30fb7c4f815f6b41fb
Jax <jbraith29@gmail.com>
2024-11-04 14:14:39 +0000

    baconnaise

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -13,7 +13,7 @@ public class App
         System.out.println(sayHelloToSomeone("Bob"));
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
-        System.out.println(sayHelloToSomeone("Jax"));
+        System.out.println(sayHelloToSomeone("Bacon"));
         System.out.println(sayGoodbye());
     }
 
